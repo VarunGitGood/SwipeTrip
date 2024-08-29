0b7994c28c82c044c4bd257dfd79c3e494718fd1
Varun Singh <varun7singh10@gmail.com>
2024-08-30 01:49:53 +0530

    fix: config

--- swipetrip/src/auth/auth.controller.ts
+++ swipetrip/src/auth/auth.controller.ts
@@ -21,6 +21,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       maxAge: 3600000,
+      sameSite: 'none',
     });
     return res.redirect(process.env.CLIENT_URL + '/dashboard');
   }
