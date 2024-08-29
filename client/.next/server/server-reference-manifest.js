e0c6baa60ca1e7feb1530f944e9460611d8b4259
Varun Singh <varun7singh10@gmail.com>
2024-08-29 20:18:28 +0530

    feat: added config

--- client/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"pqNz4hNjqEllWZSrpPUWDt+IUeKF1McT8A4fGAQ89U8=\"}"
--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NEXT_PUBLIC_API_URL: 'http://localhost:8080/api',
-    }
+  distDir: "build",
+  env: {
+    NEXT_PUBLIC_API_URL: "http://localhost:8080/api",
+  },
 };
 
 export default nextConfig;
